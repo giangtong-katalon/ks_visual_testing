9c6efee49b33357ff22fb6bb40e15686396cb4af
giangtong-katalon <giang.tong@katalon.com>
2022-08-29 22:43:10 +0700

    support headless

--- Scripts/Main Test Cases/TC_empty/Script1655275014808.groovy
+++ Scripts/Main Test Cases/TC_empty/Script1655275014808.groovy
@@ -14,11 +14,13 @@ import com.kms.katalon.core.testobject.TestObject as TestObject
 import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
-import internal.GlobalVariable as GlobalVariable
+//import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
 
 WebUI.openBrowser('https://google.com')
+
+WebUI.setViewPortSize(1366, 768)
 
 WebUI.takeScreenshotAsCheckpoint('checkpoint')
 
